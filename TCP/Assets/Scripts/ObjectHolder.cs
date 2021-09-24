f24139e9b14e821401f42cbac5c73a7a6e727b0b
Paullo B. Felipe <paullotsw@gmail.com>
2021-09-24 04:11:23 -0300

    Entediado, busquei remédio no tcp(assetRoom inici)

---
+++ TCP/Assets/Scripts/ObjectHolder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHolder : MonoBehaviour
+{
+    public static ObjectHolder instance;
+
+    Transform showTransform;
+
+    public List<GameObject> EveryObjectList = new List<GameObject>();
+    int c;
+
+
+
+    private void Awake()
+    {
+        foreach (Transform o in transform)
+        {
+            EveryObjectList.Add(o.transform.gameObject);
+
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            ReturnObj();
+        }
+
+
+    }
+
+    private void ReturnObj()
+    {
+        c++;
+        for (int i = 0; i < EveryObjectList.Count; i++)
+        {
+
+        }
+    }
+}
---
+++ TCP/Assets/Scripts/Tween.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Tween : MonoBehaviour
+{
+    public Transform[] objSelect;
+    int obj = 0;
+
+    private void Start()
+    {
+        
+    }
+
+    public void NextObject()
+    {
+        
+
+    }
+
+
+
+}

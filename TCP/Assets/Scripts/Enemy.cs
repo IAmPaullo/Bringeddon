6ff1251de6a867818c7f1c6a0aae579303dbef2b
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-10-03 19:11:30 -0300

    CUUUU

---
+++ TCP/Assets/Scripts/Enemy.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float walkDelay;
+    [SerializeField] float walkDistance;
+    Vector3 target;
+    NavMeshAgent navMesh;
+    Vector3 startPos;
+    Transform player;
+
+    [Header("Attack")]
+    [SerializeField] int maxHealth;
+    [SerializeField] int damage;
+    [SerializeField] float attackRange;
+    [SerializeField] float attackSpeed;
+    [SerializeField] float attackDuration;
+    [SerializeField] float attackCooldown;
+    [SerializeField] float perception;
+    [SerializeField] Collider attackCollider;
+    int health;
+    [Header("Debug")]
+    [SerializeField] Status status;
+    float nextAttackTime;
+    float nextWalkTime;
+    float endAttackTime;
+    float endAttackCooldown;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        startPos = transform.position;
+        status = Status.Idle;
+        navMesh = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player").transform;
+        attackCollider.enabled = false;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (status)
+        {
+            case Status.Idle:
+                anim.SetBool("Walk", false);
+                CheckPerception(); 
+                if (Time.time >= nextWalkTime)
+                {
+                    status = Status.Walk;
+                    float x = Random.Range(-walkDistance, walkDistance);
+                    float y = Random.Range(-walkDistance, walkDistance);
+                    target = new Vector3(x, 0, y) + startPos;
+                }
+                break;
+            case Status.Walk:
+                anim.SetBool("Walk", true);
+                CheckPerception();
+                if (Vector3.Distance(transform.position, target) < 0.2f)
+                {
+                    status = Status.Idle;
+                    nextWalkTime = Time.time + walkDelay;
+                }
+                break;
+            case Status.Follow:
+                anim.SetBool("Walk", true);
+                target = player.position + (transform.position - player.position).normalized;
+                if (Vector3.Distance(transform.position, player.position) > perception)
+                {
+                    status = Status.Idle;
+                    target = transform.position;
+                }
+                else if (Vector3.Distance(transform.position, player.position) < attackRange)
+                {
+                    status = Status.ToAttack;
+                    nextAttackTime = Time.time + attackSpeed;
+                }
+                break;
+            case Status.ToAttack:
+                anim.SetBool("Walk", false);
+                if (Time.time >= nextAttackTime)
+                {
+                    anim.SetTrigger("Attack");
+                    nextAttackTime = Time.time + attackSpeed;
+                    status = Status.Attacking;
+                    endAttackTime = Time.time + attackDuration;
+                    attackCollider.enabled = true;
+                }
+                break;
+            case Status.Attacking:
+                anim.SetBool("Walk", false);
+                if (Time.time >= endAttackTime)
+                {
+                    status = Status.Waiting;
+                    endAttackCooldown = Time.time + attackCooldown;
+                }
+                break;
+            case Status.Waiting:
+                anim.SetBool("Walk", false);
+                attackCollider.enabled = false;
+                if (Time.time >= endAttackCooldown)
+                {
+                    status = Status.Idle;
+                }
+                break;
+            case Status.Dead:
+                anim.SetTrigger("Death");
+                target = transform.position;
+                gameObject.layer = 10;
+                attackCollider.enabled = false;
+                break;
+        }
+        navMesh.destination = target;
+    }
+    void CheckPerception()
+    {
+        if (Vector3.Distance(transform.position,player.position) < perception)
+        {
+            status = Status.Follow;
+            target = player.position;
+        }
+    }    
+
+    public int GetDamage()
+    {
+        return damage;
+    }
+    public void ReceiveDamage(int value)
+    {
+        Debug.Log("Hit");
+        health -= value;
+        if (health <= 0)
+        {
+            health = 0;
+            Debug.Log("Dead");
+            status = Status.Dead;
+        }
+    }
+    enum Status
+    {
+        Idle, Walk, Follow, ToAttack, Attacking,
+        Waiting, Dead
+    }
+}
---
+++ TCP/Assets/Scripts/Tower.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] float range, lockTime, chargeTime, cooldown;
+    [SerializeField] int damage;
+    [SerializeField] Status status;
+    [SerializeField] Transform beam;
+    [SerializeField] Transform firePoint;
+    Transform player;
+    Vector3 target;
+    float nextLockTime, nextChargeTime, nextCooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        status = Status.Idle;
+        player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            Debug.Log(Vector3.Distance(transform.position, player.position));
+        switch (status)
+        {
+            case Status.Idle:                
+                if (Vector3.Distance(transform.position, player.position) < range)
+                {
+                    status = Status.Locking;
+                    nextLockTime = Time.time + lockTime;
+                }
+                break;
+            case Status.Locking:
+                target = player.position;
+                if (Vector3.Distance(transform.position, player.position) > range)
+                {
+                    status = Status.Idle;
+                }
+                if (Time.time >= nextLockTime)
+                {
+                    nextChargeTime = Time.time + chargeTime;
+                    status = Status.Charging;
+                }
+                break;
+            case Status.Charging:
+                if(Time.time >= nextChargeTime)
+                {
+                    status = Status.Fire;
+                }
+                break;
+            case Status.Fire:
+                Debug.Log("FIRE!!");
+                beam.position = target;
+                nextCooldown = Time.time + cooldown;
+                RaycastHit hit;
+                Vector3 dir = (target - transform.position).normalized;
+                if (Physics.Raycast(transform.position,dir,out hit))
+                {
+                    if (hit.transform.gameObject == player.gameObject)
+                        player.GetComponent<PlayerAttack>().ReceiveDamage(damage);
+                }
+                status = Status.Cooldown;
+                break;
+            case Status.Cooldown:
+                beam.position = firePoint.position;
+                if (Time.time >= nextCooldown)
+                {
+                    status = Status.Idle;
+                }
+                break;
+        }
+    }
+
+    enum Status
+    {
+        Idle, Locking, Charging, Fire, Cooldown
+    }
+}

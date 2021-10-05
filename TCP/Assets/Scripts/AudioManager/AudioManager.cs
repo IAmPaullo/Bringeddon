9d0750c0e0372c6f91f19bfc7b1de15fc892d448
Thiago_Amaro <thiagoamaro010399@gmail.com>
2021-10-05 16:47:43 -0300

    Sistema de audio

---
+++ TCP/Assets/Scripts/AudioManager/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    static AudioManager instance;
+    Audio[] audios;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        audios = Resources.LoadAll<Audio>("Audios");
+        Debug.Log(audios.Length);
+        foreach(Audio a in audios)
+        {
+            a.source = gameObject.AddComponent<AudioSource>();
+            a.source.clip = a.clip;
+            a.source.volume = a.volume;
+            a.source.loop = a.loop;
+        }
+    }
+
+    public static void Play(string _name)
+    {
+        foreach (Audio a in instance.audios)
+        {
+            if(_name == a.clipName)
+            {
+                a.source.Play();
+                return;
+            }
+        }
+    }
+}
